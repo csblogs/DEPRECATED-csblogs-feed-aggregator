44854595b24240005565a12eeceeab1c64c439e6
Daniel Brown <d.t.brown@outlook.com>
2015-04-25 01:57:04 +0100

    Added database connection code, and models for blogger and blog.

--- webjob.js
+++ webjob.js
@@ -2,23 +2,38 @@ var feed = require('feed-read');
 var http = require('http');
 var async = require('async');
 var req = require('request');
+var blogger = require('./models/blogger').Blogger;
 
-var feedUrlList = ['http://dannycomputerscientist.wordpress.com/feed',
-             'http://robcrocombe.com/feed',
-             'http://charlottegodley.co.uk/rss'];
+// Get database connection
+mongoose.connect(process.env.CUSTOMCONNSTR_MONGODB_URI || 'mongodb://localhost');
 
-async.each(feedUrlList,
-    function(feedUrl, done) {
-        feed(feedUrl, function(err, articles) {
-            dealWithParsedFeed(articles);
-            done();        
-        });       
-    }, 
-    function(err) {
-        console.log('All downloaded');
+var database = mongoose.connection;
+database.on('error', console.error.bind(console, 'MongoDB Connection Error:'));
+database.once('open', function (callback) {
+    console.log('Database connection established successfully.')
+    
+    // Get all feed URLs
+
+    // Download all RSS/ATOM feeds and process them.
+    async.each(feedUrlList,
+        function(feedUrl, done) {
+            feed(feedUrl, function(err, articles) {
+                dealWithParsedFeed(articles);
+                done();        
+            });       
+        }, 
+        function(err) {
+            if(!err) {
+                console.log('All feeds downloaded, parsed and new items added to db');
+            }
+            else {
+                console.error('[FATAL] %j', err);        
+            }
+        }
+    );
+    
+    function dealWithParsedFeed(articles) {
+        console.log(JSON.stringify(articles, null, 4));
     }
-);
+});
 
-function dealWithParsedFeed(articles) {
-    console.log(JSON.stringify(articles, null, 4));
-}
