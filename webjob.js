1288c3c68343278bfcd371d97df294d3dcb2a3eb
Daniel Brown <d.t.brown@outlook.com>
2015-04-25 02:30:09 +0100

    Can now get all bloggers from DB

--- webjob.js
+++ webjob.js
@@ -13,27 +13,42 @@ database.once('open', function (callback) {
     console.log('Database connection established successfully.')
     
     // Get all feed URLs
-
-    // Download all RSS/ATOM feeds and process them.
-    async.each(feedUrlList,
-        function(feedUrl, done) {
-            feed(feedUrl, function(err, articles) {
-                dealWithParsedFeed(articles);
-                done();        
-            });       
-        }, 
-        function(err) {
-            if(!err) {
-                console.log('All feeds downloaded, parsed and new items added to db');
-            }
-            else {
-                console.error('[FATAL] %j', err);        
-            }
-        }
-    );
-    
-    function dealWithParsedFeed(articles) {
-        console.log(JSON.stringify(articles, null, 4));
-    }
+    blogger.find({}, function(error, allBloggers) {
+        if (error) {
+			console.log("[FATAL] %j", err);
+		}
+		else {
+			if(!allBloggers) {
+				console.log("[WARN] No bloggers in DB");
+			}
+			else {
+				downloadAllFeeds(allBloggers);
+			}
+		}
+	});
 });
 
+// Download all RSS/ATOM feeds and process them.
+function downloadAllFeeds(allBloggers) {
+	async.each(allBloggers,
+	    function(blogger, done) {
+	        feed(blogger.feedUrl, function(err, articles) {
+	            dealWithParsedFeed(articles);
+	            done();        
+	        });       
+	    }, 
+	    function(err) {
+	        if(!err) {
+	            console.log('All feeds downloaded, parsed and new items added to db');
+	        }
+	        else {
+	            console.error('[FATAL] %j', err);        
+	        }
+	    }
+	);
+}
+
+
+function dealWithParsedFeed(articles) {
+    console.log(JSON.stringify(articles, null, 4));
+}
